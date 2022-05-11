9f9779a5c48a3a9ebc9ca57cf7166c88e46ae1db
mohtsuji <ohtsuji.mizuki@gmail.com>
2022-05-11 17:57:01 +0900

    finish step3-1

--- go/app/main.go
+++ go/app/main.go
@@ -27,13 +27,13 @@ func root(c echo.Context) error {
 
 func addItem(c echo.Context) error {
 	// Get form data
-	name := c.FormValue("name")
+	name := c.FormValue("name") //多分 -d name=jacketとしたときには，新たにjacketという名前のフォーム（カテゴリ？）を作成している？
 	c.Logger().Infof("Receive item: %s", name)
 
-	message := fmt.Sprintf("item received: %s", name)
-	res := Response{Message: message}
+	message := fmt.Sprintf("item received: %s", name) //これは画面に表示しているだけ
+	res := Response{Message: message}                 //サーバーがレスポンスするメッセージを指定
 
-	return c.JSON(http.StatusOK, res)
+	return c.JSON(http.StatusOK, res) // レスポンスをJSONに変換して表示？
 }
 
 func getImg(c echo.Context) error {
@@ -52,27 +52,33 @@ func getImg(c echo.Context) error {
 }
 
 func main() {
+	//new instance
 	e := echo.New()
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Logger.SetLevel(log.INFO)
+	// Middlewareの設定
+	//https://ken-aio.github.io/post/2019/02/06/golang-echo-middleware/
+	e.Use(middleware.Logger())  //いわゆるアクセスログのようなリクエスト単位のログを出力
+	e.Use(middleware.Recover()) //アプリケーションのどこかで予期せずにpanicを起こしてしまっても、サーバは落とさずにエラーレスポンスを返せるようにリカバリーする
+	//ログの出力レベルを設定
+	e.Logger.SetLevel(log.INFO) //INFOはlog level 2らしいが，具体的にはわからない・・・
 
-	front_url := os.Getenv("FRONT_URL")
+	front_url := os.Getenv("FRONT_URL") //"FROTN_URL"は空なのでifに入る
 	if front_url == "" {
-		front_url = "http://localhost:3000"
+		front_url = "http://localhost:3000" //このport番号って適当？
 	}
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{front_url},
+		AllowOrigins: []string{front_url}, //今回はfront_urlに対して情報共有を許可しますよ，ということ
 		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+		//使用可能はメソッドはGET,PUT,POST,DELTE
+		//AllowCredentialsは初期値でfalse(cookieは使用しない）)
 	}))
 
 	// Routes
-	e.GET("/", root)
+	// 各ルーティングに対するハンドラを設定
+	e.GET("/", root) //"/"がきたらrootをよぶ
 	e.POST("/items", addItem)
 	e.GET("/image/:itemImg", getImg)
 
 	// Start server
-	e.Logger.Fatal(e.Start(":9000"))
+	e.Logger.Fatal(e.Start(":9000")) //Loggerはinterfaseだった。多分このinterfaceの実装はecho.goがimportしている"github.com/labstack/gommon/log"の中で実装されている。しかしこの中でもさらにglobalとかいう変数を使っててもうわけわかめ
 }
