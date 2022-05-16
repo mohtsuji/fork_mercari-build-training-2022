c0f01a76385c6a15d2eae0e9ae8c8e0791d08d0c
mohtsuji <ohtsuji.mizuki@gmail.com>
2022-05-17 01:06:02 +0900

    items.jsonの名前が間違っていたので修正＆items.jsonを追記するように変更＆getItem()はitems.jsonからデータを取得するように変更

--- go/app/main.go
+++ go/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
@@ -35,57 +37,104 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
-var itemData = ItemData{Items: make([]Item, 0, 0)}
+type MyStr string
+
+func (s MyStr) isEmpty() bool {
+	return s == ""
+}
+
+var DbConnection *sql.DB
 
 func ErrLackItem(field string, c echo.Context) error {
-	c.Logger().Warn(`Failed to create "item.json": `, field, " is ", "empty.") //Output Warn log for developers
+	c.Logger().Warn(`Failed to create "items.json": `, field, " is ", "empty.") //Output Warn log for developers
 	message := fmt.Sprintf(`Failed to create item. Please fill %s.`, field)
 	res := Response{Message: message}
 	return c.JSON(http.StatusBadRequest, res) //Response for user
 }
 
 func FailCreateItem(err error, c echo.Context) error {
-	c.Logger().Error(`Failed to create "item.json": %v`, err) //Output Warn log for developers
+	c.Logger().Error(`Failed to create "items.json": %v`, err) //Output Warn log for developers
 	res := Response{Message: `Failed to create item`}
 	return c.JSON(http.StatusInternalServerError, res) //Response for user
 }
 
+func FailGetItem(err error, c echo.Context) error {
+	c.Logger().Error(`Failed to get "items.json": %v`, err) //Output Warn log for developers
+	res := Response{Message: `Failed to get item`}
+	return c.JSON(http.StatusInternalServerError, res) //Response for user
+}
+
+func readItemJSON(c echo.Context) (ItemData, error) {
+	save_items := ItemData{}
+	read_items_byte, err := os.ReadFile("items.json")
+	if err != nil {
+		return save_items, err
+	}
+	if len(read_items_byte) != 0 {
+		err = json.Unmarshal(read_items_byte, &save_items)
+		if err != nil {
+			return save_items, err
+		}
+	}
+	return save_items, nil
+}
+
 func addItem(c echo.Context) error {
-	//fileの作成
-	file, err := os.Create("item.json") //file->type os.File
+	// Get form data
+	name := MyStr(c.FormValue("name")) //Create Form Value
+	category := MyStr(c.FormValue("category"))
+	//If "name" or "category" is lack, it is error
+	if name.isEmpty() == true {
+		return ErrLackItem("name", c)
+	}
+	c.Logger().Infof("Receive item: %s", name)
+	if category.isEmpty() == true {
+		return ErrLackItem("category", c)
+	}
+	c.Logger().Infof("Receive item: %s", category)
+
+	//read "items.json"
+	file, err := os.OpenFile("items.json", os.O_RDWR|os.O_CREATE, 0775) //file->type os.File
 	if err != nil {
 		return FailCreateItem(err, c)
 	}
 	defer file.Close()
-
-	// Get form data
-	name := c.FormValue("name") //Create Form Value
-	category := c.FormValue("category")
-	//If "name" or "category" is lack, it is error
-	if name == "" {
-		return ErrLackItem("name", c)
+	save_items, err := readItemJSON(c)
+	if err != nil {
+		return FailCreateItem(err, c)
 	}
-	c.Logger().Infof("Receive item: %s", name)
-	if category == "" {
-		return ErrLackItem("category", c)
-	}
-	c.Logger().Infof("Receive item: %s", category)
-	itemData.Items = append(itemData.Items, Item{Name: name, Category: category})
+	save_items.Items = append(save_items.Items, Item{Name: string(name), Category: string(category)})
 
+	//encoding and write contents to "items.json"
 	data := json.NewEncoder(file) //output stream = file
 	data.SetIndent("", " ")       //set Indent
-	err = data.Encode(itemData)   //Go→JSON(encoding)
+	err = data.Encode(save_items) //Go→JSON(encoding)
 	if err != nil {
 		return FailCreateItem(err, c)
 	}
 
+	//connect database
+	/*	DbConnection, err := sql.Open("sqlite3", "../db/mercari.sql")
+		if err != nil {
+			//		return FailCreateDatabase(err, c)
+		}
+		defer DbConnection.Close()
+		err = DbConnection.Ping()
+		if err != nil {
+			//	log.Fatal(err)
+		}
+	*/
 	message := fmt.Sprintf("item received: %s", name)
 	res := Response{Message: message}
 	return c.JSON(http.StatusOK, res) //Response for user
 }
 
 func getItem(c echo.Context) error {
-	return c.JSONPretty(http.StatusOK, itemData, " ") //Go言語仕様のままでも勝手にJSONにエンコーディングしてくれる
+	save_items, err := readItemJSON(c)
+	if err != nil {
+		return FailGetItem(err, c)
+	}
+	return c.JSONPretty(http.StatusOK, save_items, " ") //Go言語仕様のままでも勝手にJSONにエンコーディングしてくれる
 }
 
 func getImg(c echo.Context) error {
@@ -111,7 +160,7 @@ func main() {
 	//https://ken-aio.github.io/post/2019/02/06/golang-echo-middleware/
 	e.Use(middleware.Logger())  //いわゆるアクセスログのようなリクエスト単位のログを出力
 	e.Use(middleware.Recover()) //アプリケーションのどこかで予期せずにpanicを起こしてしまっても、サーバは落とさずにエラーレスポンスを返せるようにリカバリーする
-	//ログの出力レベルを設定
+	//set log level
 	e.Logger.SetLevel(log.INFO) //e.LoggerはINFO以上のログじゃないと出力しない
 
 	front_url := os.Getenv("FRONT_URL") //"FROTN_URL"は空なのでifに入る
